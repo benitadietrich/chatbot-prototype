4c9ff7d00f3a433847e44fea503bb4fd04059618
Benita <benita.dietrich@web.de>
2021-08-01 16:03:15 +0200

    updated

--- frontend/src/CustomWidget.jsx
+++ frontend/src/CustomWidget.jsx
@@ -3,10 +3,10 @@ import Widget from "rasa-webchat";
 export default function CustomWidget() {
   return (
     <Widget
-      initPayload={"Hi"}
+      initPayload={"start"}
       socketUrl={"http://localhost:5005"}
       socketPath={"/socket.io/"}
-      customData={{ language: "de" }} // arbitrary custom data. Stay minimal as this will be added to the socket
+      customData={{ language: "de", storage: "session" }} // arbitrary custom data. Stay minimal as this will be added to the socket
       title={"Studienberatung"}
       storage="session"
     />
--- rasa-bot/actions/actions.py
+++ rasa-bot/actions/actions.py
@@ -81,3 +81,114 @@
     #         # validation failed, set this slot to None so that the
     #         # user will be asked for the slot again
     #         return {"criticalThinking": None}
+
+
+from typing import Text, List, Any, Dict
+
+from rasa_sdk import Tracker, FormValidationAction, Action
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from rasa_sdk.events import SlotSet
+from rasa_sdk.knowledge_base.storage import InMemoryKnowledgeBase
+from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
+
+
+class ValidatePersonalityForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_personality_form"
+
+    # def validate_thinking(
+    #     self,
+    #     slot_value: Any,
+    #     dispatcher: CollectingDispatcher,
+    #     tracker: Tracker,
+    #     domain: DomainDict,
+    # ) -> Dict[Text, Any]:
+    #     """Validate thinking value."""
+    #     intent = tracker.latest_message['intent'].get('name')
+    #     SlotSet("context","critical")
+    #     currentResult = tracker.get_slot('result')
+    #     dispatcher.utter_message(template="utter_critical")
+
+    #     if intent == "critical_bad":
+    #         dispatcher.utter_message(template="utter_critical_bad")
+    #         return {"result": currentResult-1}
+    #     else:
+    #         # validation failed, set this slot to None so that the
+    #         # user will be asked for the slot again
+    #         dispatcher.utter_message(template="utter_critical_good")
+    #         return {"result": currentResult+1}
+
+    def create_validation_function(name_of_slot):
+        """Function generate our validation functions, since
+        they're pretty much the same for each slot"""
+
+        def validate_slot(
+            self,
+            value: Text,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+        ) -> Dict[Text, Any]:
+
+            """Validate thinking value."""
+            intent = tracker.latest_message['intent'].get('name')
+            currentResult = tracker.get_slot('result')
+
+            if intent == name_of_slot+"_bad":
+                dispatcher.utter_message(response="utter_"+name_of_slot+"_bad")
+                return {"result": currentResult-1}
+            else:
+                dispatcher.utter_message(response="utter_"+name_of_slot+"_good")
+                return {"result": currentResult+1}
+
+        return validate_slot
+
+    validate_thinking = create_validation_function(name_of_slot="thinking")
+    validate_organization = create_validation_function(name_of_slot="organization")
+
+class ActionResult(Action):
+
+  def name(self) -> Text:
+      return "action_result"
+
+  async def run(
+      self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
+  ) -> List[Dict[Text, Any]]:
+
+      result = tracker.get_slot('result')
+      dispatcher.utter_message("Hallo")
+      message = ""
+      quality = ""
+
+      if result >= 7:
+          message = "Danke für deine Antworten. Du scheinst viele Fähigkeiten zu besitzen die für ein duales Studium wichtig sind, ein Duales Studium scheint für dich geeignet zu sein."
+          quality = "good"
+      elif result <7 and result >= 5:
+          message = "Danke für deine Antworten. Du besitzt schon einige Fähigkeiten zu besitzen, die es für ein duales Studium braucht. Allerdings gibt es auch einige Themenbereiche, bei denen du dich verbessern könntest."
+          quality = "medium"
+      else:
+          message = "Danke für deine Antworten. Dein Ergebnis scheint etwas kontrovers zu sein, vielleicht ist eine andere Form des Studiums für dich eher geeignet. Wenn du möchtest, kannst du gerne ein persönliches Gespräch vereinbaren um mehr zu erfahren"
+          quality= "bad"
+
+      dispatcher.utter_message(message)
+
+      return {quality: quality}
+
+class ActionAsk(Action):
+
+    def name(self) -> Text:
+        return "action_ask"
+
+    async def run(
+      self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
+  ) -> List[Dict[Text, Any]]:
+
+     context = tracker.get_slot('requested_slot')
+     dispatcher.utter_message(response="utter_"+context)
+     return[]
+class MyKnowledgeBaseAction(ActionQueryKnowledgeBase):
+
+    def __init__(self):
+        knowledge_base = InMemoryKnowledgeBase(".\data.json")
+        super().__init__(knowledge_base)
