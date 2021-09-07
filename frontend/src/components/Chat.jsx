4d8f1fec083ff09b40e797fe73d6cacd3967eee2
Benita <benita.dietrich@web.de>
2021-09-07 15:47:15 +0200

    delete

--- frontend/src/components/Chat.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import { Popover } from '@material-ui/core';
-export default class Chat extends Component {
-  render() {
-    return (
-     <div></div>
-    );
-  }
-}
--- frontend/src/components/Page.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export const Page = () => {
-    return(
-        <div>
-            <h1>Willkommen!</h1>
-        </div>
-    )
-}
