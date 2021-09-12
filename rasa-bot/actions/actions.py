e8bef9b8dbd1d21d22b03578c1525f3308e3ea52
Benita <benita.dietrich@web.de>
2021-09-12 15:06:41 +0200

    fix

--- rasa-bot/actions/actions.py
+++ rasa-bot/actions/actions.py
@@ -252,19 +252,20 @@ class ActionResetAllSlots(Action):
 class ActionWannaStudy(Action):
 
     def name(self) -> Text:
-	    return "action_wanna_study"
+        return "action_wanna_study"
 
     async def run(
-	self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
+        self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
     ) -> List[Dict[Text, Any]]:
 
-    	studiengang = tracker.get_slot('object_type')
+        studiengang = tracker.get_slot('object_type')
 
         if studiengang is None:
-		    dispatcher.utter_message(response = "utter_ask_studiengang")
+            dispatcher.utter_message(response="utter_ask_studiengang")
         else:
-		    dispatcher.utter_message(response = "utter_"+studiengang)
+            dispatcher.utter_message(response="utter_"+studiengang.lower())
 
+        return[]
 
 class ActionSearchDatabase(Action):
 
